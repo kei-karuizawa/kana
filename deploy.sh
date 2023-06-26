a0dc46b4ea05d2c2bd7c93d10e72e4af6ecbce04
河瀬雫 <shizuku@himari.local>
2023-06-26 18:33:14 +0800

    update：添加一些秘钥。

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 
-push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:神流歌奈/vuepress-theme-vdoing.git
+push_addr=`https://github.com/Otsuha/kana.git` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:神流歌奈/vuepress-theme-vdoing.git
 commit_info=`git describe --all --always --long`
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=gh-pages # 推送的分支
+push_branch=main # 推送的分支
 
 # 生成静态文件
 npm run build
