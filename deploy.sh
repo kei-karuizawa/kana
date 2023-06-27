72a95e65c03b95b556b41f0f5c77804ded5a675d
Otsuha <grass.ichika@gmail.com>
2023-06-27 21:38:20 +0800

    update：更新提交脚本。

--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,24 @@
 #!/usr/bin/env sh
+
 # 确保脚本抛出遇到的错误
 set -e
-npm run build # 生成静态文件
-cd docs/.vuepress/dist # 进入生成的文件夹
 
-# deploy to github
-#echo 'blog.xugaoyi.com' > CNAME
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:Otsuha/kana.git
-else
-  msg='来自github action的自动部署'
-  githubUrl=https://Otsuha:${GITHUB_TOKEN}@github.com/Otsuha/kana.git
-  git config --global user.name "Otsuha"
-  git config --global user.email "grass.ichika@gmail.com"
-  git config --global init.defaultBranch gh-pages
-fi
+
+push_addr=git@github.com:Otsuha/kana.git # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+commit_info=deploy
+dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
+push_branch=gh-pages # 推送的分支
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd $dist_path
+
 git init
 git add -A
-git commit -m "${msg}"
-git push -f $githubUrl main:gh-pages --force # 推送到github
-
-# deploy to coding
-# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@git.dev.tencent.com:xugaoyi/xugaoyi.git
-# else
-#   codingUrl=https://xugaoyi:${CODING_TOKEN}@git.dev.tencent.com/xugaoyi/xugaoyi.git
-#fi
-# git add -A
-# git commit -m "${msg}"
-# git push -f $codingUrl master # 推送到coding
+git commit -m "deploy, $commit_info"
+git push -f $push_addr HEAD:$push_branch
 
 cd -
-rm -rf docs/.vuepress/dist
+rm -rf $dist_path
